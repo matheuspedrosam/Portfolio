0a592cdda5eac20dfc4d0313781538fc3466b80d
matheuspedrosam <matheuspedrosa2002@gmail.com>
2022-08-29 11:46:48 -0300

    corrigindo responsividade 2

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 var header = document.getElementById('header')
 var menu_hamburguer = document.getElementById('menu')
+var imagem_menu = document.getElementById('img-menu')
 
 function abrirMenu(){
     if (header.style.display == 'flex'){
         header.style.display = 'none'
         menu_hamburguer.innerHTML = 'menu'
+        imagem_menu.style.display = 'inline-block'
     } else {
+        imagem_menu.style.display = 'none'
+        header.style.display = 'flex'
+        menu_hamburguer.innerHTML = 'close'
+    }
+}
+
+function ajeitarMenu(){
+    if (window.innerWidth >= 800){
+        imagem_menu.style.display = 'none'
         header.style.display = 'flex'
         menu_hamburguer.innerHTML = 'close'
     }
