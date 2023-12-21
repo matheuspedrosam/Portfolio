84b24030cfbfb034666068f44a9b5f4a2778bd52
matheuspedrosam <matheuspedrosa2002@gmail.com>
2023-12-21 07:48:29 -0300

    update main-section and added a Youtube button

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ var header = document.getElementById('header')
 var menu_hamburguer = document.getElementById('menu')
 var imagem_menu = document.getElementById('img-menu')
 
+
+function fecharMenuAoScrollar(){
+    if (scrollY > 10){
+        subirMenu()
+        console.log("entrou")
+    }
+}
+
 function abrirMenu(){
     if (header.style.display == 'flex'){
+        window.removeEventListener("scroll", fecharMenuAoScrollar)
         header.style.display = 'none'
         menu_hamburguer.innerHTML = 'menu'
         imagem_menu.style.display = 'inline-block'
     } else {
+        window.addEventListener("scroll", fecharMenuAoScrollar)
         imagem_menu.style.display = 'none'
         header.style.display = 'flex'
         menu_hamburguer.innerHTML = 'close'
@@ -30,6 +40,7 @@ function ajeitarMenu(){
 function subirMenu(){
     if (window.innerWidth <= 800){
         if (header.style.display == 'flex'){
+            window.removeEventListener("scroll", fecharMenuAoScrollar)
             header.style.display = 'none'
             menu_hamburguer.innerHTML = 'menu'
             imagem_menu.style.display = 'block'
